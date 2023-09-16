aa5da25610a378acb2136f3da9027d5282f90e21
Roman <ramzes311982@gmail.com>
2023-09-16 21:05:36 +0300

    add img

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 window.addEventListener('DOMContentLoaded', function() {
+
+  // const tastes = document.querySelectorAll('.description_tastes');
+  // let img = document.innerText = `<img src="img/Logo.svg" alt="Big Logo">`;
+
+  // tastes.forEach((e) => {
+  // e.prepend(img)
+  //   console.log(e);
+  // }) 
   
   
  
   
   });
   
+
