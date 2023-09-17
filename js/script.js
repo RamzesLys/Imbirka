3f92bd6399a8cfa1405b5c592ef50a33af39c620
Roman <ramzes311982@gmail.com>
2023-09-17 20:57:34 +0300

    validation

--- js/script.js
+++ js/script.js
@@ -7,10 +7,23 @@ window.addEventListener('DOMContentLoaded', function() {
   // e.prepend(img)
   //   console.log(e);
   // }) 
-  
-  
+
  
   
   });
   
 
+
+  let formName = document.querySelector('.form_name');
+  let formPhone = document.querySelector('.form_phone');
+  
+  const btn = document.querySelector('.form_btn')
+
+  btn.addEventListener('click', () => {
+   event.preventDefault();
+   if (formPhone.value !== typeof('number') || formPhone.lenght <= 7) {
+    console.log(`Шановний(на) ${formName.value}, введіть коректний номер телефону`);
+   }
+    console.log(`Ім'я ${formName.value}, телефон ${formPhone.value}`);
+  })
+  
