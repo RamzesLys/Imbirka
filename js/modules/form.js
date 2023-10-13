bc410b3f85926a985463ddb87482b85d2de4b2ea
Roman <ramzes311982@gmail.com>
2023-10-13 22:08:52 +0300

    form phone +

--- js/modules/form.js
+++ js/modules/form.js
@@ -4,9 +4,6 @@ function formShow() {
   const form = document.querySelector('.form'),
         btn = document.querySelector('button[type=submit]'),
         btnClose = document.querySelector('.form_btn-close');
-  let formName = document.querySelector('.form_name');
-  let formPhone = document.querySelector('.form_phone');
-  let formMessage = document.querySelector('.form_message');
    
   setTimeout(() => {
     form.style.display = 'inherit';
